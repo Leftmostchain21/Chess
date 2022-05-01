9a10a8338ecd8559b74fb28f002cd40a050d358e
Thomas Massey <thomassey476@outlook.com>
2022-05-01 13:23:34 +0100

    Transparrent background

--- renderer.py
+++ renderer.py
@@ -1,5 +1,7 @@
 # This file will render the chess board and pieces using pygame.
 
+import colorama
+from numpy import imag
 import pygame
 import GameEngine
 
@@ -18,17 +20,21 @@ BLUE = (0, 0, 255)
 IMAGES = {}
 
 
+surface = pygame.display.set_mode((WIDTH, HEIGHT))
+
 # Create a function that will draw the board
 
 def load_Images():
     pieces = ["Nb", "Rb", "Bb", "Qb", "Kb", "Pb", "Pw", "Rw", "Nw", "Kw", "Qw", "Bw"]
     for piece in pieces:
-        IMAGES[piece] = pygame.transform.scale(pygame.image.load("Chess/images/" + piece + ".png"), (WIDTH / 8, HEIGHT / 8))
-    # We can now access an image by saying 'IMAGES['wp']'
+        # Load the image and then convert it to be a transparent png
+        image_loading = pygame.image.load("Chess/images/" + piece + ".png").convert_alpha()
+        IMAGES[piece] = pygame.transform.scale(image_loading, (WIDTH / 8, HEIGHT / 8))
+    # We can now access an image by saying 'IMAGES['Pw']'
 
 def draw_board():
     pygame.init()
-    surface = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Chess")
     # Draw the board
     for row in range(8):
         for col in range(8):
@@ -39,12 +45,11 @@ def draw_board():
     pygame.display.update()
 
 def draw_pieces(board):
-    surface = pygame.display.set_mode((WIDTH, HEIGHT))
     # Draw the pieces
     for row in range(8):
         for col in range(8):
-            if board[col][row][-1:] == "w":
-                surface.blit(IMAGES[board[col][row]], pygame.Rect(col * WIDTH / 8, row * HEIGHT / 8, WIDTH / 8, HEIGHT / 8))
-            elif board[col][row][-1:] == "b":
-                surface.blit(IMAGES[board[col][row]], pygame.Rect(col * WIDTH / 8, row * HEIGHT / 8, WIDTH / 8, HEIGHT / 8))
+            if board[row][col][-1:] == "w":
+                surface.blit(IMAGES[f"{board[row][col]}"], pygame.Rect(col * WIDTH / 8, row * HEIGHT / 8, WIDTH / 8, HEIGHT / 8))
+            elif board[row][col][-1:] == "b":
+                surface.blit(IMAGES[f"{board[row][col]}"], pygame.Rect(col * WIDTH / 8, row * HEIGHT / 8, WIDTH / 8, HEIGHT / 8))
     pygame.display.update()
